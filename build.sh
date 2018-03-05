da74cdf19cf5a5621092de6a10b4853bccd807b6
Yannick Bröker <ybroeker@techfak.uni-bielefeld.de>
2018-03-05 15:32:39 +0100

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 git submodule init
-git submodule update
+git submodule update --remote
 
 mkdir html && cp -r adoc/img html || cp -r adoc/img/* html/img
 asciidoctor -D html -r asciidoctor-diagram adoc/index.adoc
