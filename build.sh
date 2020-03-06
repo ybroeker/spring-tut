c7abf6b8d2c672d85e3d2960117f6b55d94448e0
Yannick Bröker <ybroeker@techfak.uni-bielefeld.de>
2020-03-06 22:04:27 +0100

    Update tut

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 git submodule init
 git submodule update --remote
 
-mkdir html && cp -r adoc/img html || cp -r adoc/img/* html/img
+mkdir html
+cp -r adoc/img html || cp -r adoc/img/* html/img
 asciidoctor -D html -r asciidoctor-diagram adoc/index.adoc
